5140ae5d5a356c6af7ea69fb8eb5bf91e2a86e78
mytelegram <mytelegramserver@outlook.com>
2024-04-24 20:32:21 +0800

    fix: Add new parameter

--- source/src/MyTelegram.Messenger/TLObjectConverters/Interfaces/IPeerSettingsConverter.cs
+++ source/src/MyTelegram.Messenger/TLObjectConverters/Interfaces/IPeerSettingsConverter.cs
@@ -4,5 +4,5 @@ namespace MyTelegram.Messenger.TLObjectConverters.Interfaces;
 
 public interface IPeerSettingsConverter : ILayeredConverter
 {
-    IPeerSettings ToPeerSettings(long targetUserId, IPeerSettingsReadModel? readModel, ContactType? contactType);
+    IPeerSettings ToPeerSettings(long selfUserId, long targetUserId, IPeerSettingsReadModel? readModel, ContactType? contactType);
 }
