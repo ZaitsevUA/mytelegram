b93957ff82bb4e54f87e3bce8e197caafc9f5ec2
mytelegram <mytelegramserver@outlook.com>
2024-07-06 14:13:40 +0800

    fix: ReadHistory

--- source/src/MyTelegram.Messenger/Handlers/LatestLayer/Impl/Messages/ReadHistoryHandler.cs
+++ source/src/MyTelegram.Messenger/Handlers/LatestLayer/Impl/Messages/ReadHistoryHandler.cs
@@ -1,52 +1,45 @@
 ﻿// ReSharper disable All
 
+using RequestReadHistory = MyTelegram.Schema.Messages.RequestReadHistory;
+
 namespace MyTelegram.Handlers.Messages;
 
-///<summary>
-/// Marks message history as read.
-/// <para>Possible errors</para>
-/// Code Type Description
-/// 400 CHANNEL_PRIVATE You haven't joined this channel/supergroup.
-/// 400 CHAT_ID_INVALID The provided chat id is invalid.
-/// 400 MSG_ID_INVALID Invalid message ID provided.
-/// 400 PEER_ID_INVALID The provided peer id is invalid.
-/// See <a href="https://corefork.telegram.org/method/messages.readHistory" />
-///</summary>
-internal sealed class ReadHistoryHandler : RpcResultObjectHandler<MyTelegram.Schema.Messages.RequestReadHistory, MyTelegram.Schema.Messages.IAffectedMessages>,
-    Messages.IReadHistoryHandler
+/// <summary>
+///     Marks message history as read.
+///     <para>Possible errors</para>
+///     Code Type Description
+///     400 CHANNEL_PRIVATE You haven't joined this channel/supergroup.
+///     400 CHAT_ID_INVALID The provided chat id is invalid.
+///     400 MSG_ID_INVALID Invalid message ID provided.
+///     400 PEER_ID_INVALID The provided peer id is invalid.
+///     See <a href="https://corefork.telegram.org/method/messages.readHistory" />
+/// </summary>
+internal sealed class ReadHistoryHandler(
+    ICommandBus commandBus,
+    IPeerHelper peerHelper,
+    IAccessHashHelper accessHashHelper,
+    IQueryProcessor queryProcessor,
+    IPtsHelper ptsHelper)
+    :
+        RpcResultObjectHandler<RequestReadHistory, Schema.Messages.IAffectedMessages>,
+        Messages.IReadHistoryHandler
 {
-    private readonly ICommandBus _commandBus;
-    private readonly IPeerHelper _peerHelper;
-    private readonly IAccessHashHelper _accessHashHelper;
-    private readonly IQueryProcessor _queryProcessor;
-    private readonly IPtsHelper _ptsHelper;
-    public ReadHistoryHandler(ICommandBus commandBus,
-        IPeerHelper peerHelper,
-        IAccessHashHelper accessHashHelper, IQueryProcessor queryProcessor, IPtsHelper ptsHelper)
-    {
-        _commandBus = commandBus;
-        _peerHelper = peerHelper;
-        _accessHashHelper = accessHashHelper;
-        _queryProcessor = queryProcessor;
-        _ptsHelper = ptsHelper;
-    }
-
     protected override async Task<IAffectedMessages> HandleCoreAsync(IRequestInput input,
-        MyTelegram.Schema.Messages.RequestReadHistory obj)
+        RequestReadHistory obj)
     {
-        await _accessHashHelper.CheckAccessHashAsync(obj.Peer);
+        await accessHashHelper.CheckAccessHashAsync(obj.Peer);
         if (obj.MaxId < 0)
         {
             return new TAffectedMessages
             {
-                Pts = _ptsHelper.GetCachedPts(input.UserId),
+                Pts = ptsHelper.GetCachedPts(input.UserId),
                 PtsCount = 0
             };
         }
 
-        var peer = _peerHelper.GetPeer(obj.Peer, input.UserId);
+        var peer = peerHelper.GetPeer(obj.Peer, input.UserId);
         var messageReadModel =
-            await _queryProcessor.ProcessAsync(
+            await queryProcessor.ProcessAsync(
                 new GetMessageByIdQuery(MessageId.Create(input.UserId, obj.MaxId).Value));
 
         if (messageReadModel == null)
@@ -55,33 +48,22 @@ internal sealed class ReadHistoryHandler : RpcResultObjectHandler<MyTelegram.Sch
         }
 
         var selfDialogId = DialogId.Create(input.UserId, peer);
-        var dialogReadModel = await _queryProcessor.ProcessAsync(new GetDialogByIdQuery(selfDialogId));
+        var dialogReadModel = await queryProcessor.ProcessAsync(new GetDialogByIdQuery(selfDialogId));
         if (dialogReadModel == null || dialogReadModel.ReadInboxMaxId >= obj.MaxId)
         {
             return new TAffectedMessages
             {
-                Pts = _ptsHelper.GetCachedPts(input.UserId),
+                Pts = ptsHelper.GetCachedPts(input.UserId),
                 PtsCount = 0
             };
         }
 
-        var command = new UpdateReadInboxMaxIdCommand(selfDialogId, input.ToRequestInfo(), obj.MaxId, messageReadModel!.SenderUserId, messageReadModel.SenderMessageId);
-        await _commandBus.PublishAsync(command);
+        var unreadCount =
+            await queryProcessor.ProcessAsync(new GetUnreadCountQuery(input.UserId, peer.PeerId, obj.MaxId));
 
-        //var unreadCount =
-        //    await _queryProcessor.ProcessAsync(new GetUnreadCountQuery(input.UserId, peer.PeerId, obj.MaxId));
-
-        //var readInboxMessageCommand = new ReadInboxMessageCommand2(selfDialogId,
-        //    input.ToRequestInfo(),
-        //    input.UserId,
-        //    input.UserId,
-        //    obj.MaxId,
-        //    unreadCount,
-        //    peer,
-        //    CurrentDate
-        //    );
-
-        //await _commandBus.PublishAsync(readInboxMessageCommand, default);
+        var command = new UpdateReadInboxMaxIdCommand(selfDialogId, input.ToRequestInfo(), obj.MaxId,
+            messageReadModel!.SenderUserId, messageReadModel.SenderMessageId, unreadCount);
+        await commandBus.PublishAsync(command);
 
         return null!;
     }
