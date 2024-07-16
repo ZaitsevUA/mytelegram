8adfb113cc054dac604a67f9c8eada206f92f4b3
mytelegram <mytelegramserver@outlook.com>
2024-07-16 12:15:01 +0800

    fix: GetContacts

--- source/src/MyTelegram.Messenger/Handlers/LatestLayer/Impl/Contacts/GetContactsHandler.cs
+++ source/src/MyTelegram.Messenger/Handlers/LatestLayer/Impl/Contacts/GetContactsHandler.cs
@@ -6,48 +6,47 @@ namespace MyTelegram.Handlers.Contacts;
 /// Returns the current user's contact list.
 /// See <a href="https://corefork.telegram.org/method/contacts.getContacts" />
 ///</summary>
-internal sealed class GetContactsHandler : RpcResultObjectHandler<MyTelegram.Schema.Contacts.RequestGetContacts, MyTelegram.Schema.Contacts.IContacts>,
-    Contacts.IGetContactsHandler
+internal sealed class GetContactsHandler(
+    IQueryProcessor queryProcessor,
+    ILayeredService<IUserConverter> layeredUserService,
+    IPhotoAppService photoAppService,
+    IHashCalculator hashCalculator,
+    IPrivacyAppService privacyAppService)
+    : RpcResultObjectHandler<MyTelegram.Schema.Contacts.RequestGetContacts, MyTelegram.Schema.Contacts.IContacts>,
+        Contacts.IGetContactsHandler
 {
-    private readonly IQueryProcessor _queryProcessor;
-    private readonly ILayeredService<IUserConverter> _layeredUserService;
-    private readonly IPhotoAppService _photoAppService;
-    private readonly IPrivacyAppService _privacyAppService;
-
-    public GetContactsHandler(IQueryProcessor queryProcessor,
-        ILayeredService<IUserConverter> layeredUserService, IPhotoAppService photoAppService, IPrivacyAppService privacyAppService)
-    {
-        _queryProcessor = queryProcessor;
-        _layeredUserService = layeredUserService;
-        _photoAppService = photoAppService;
-        _privacyAppService = privacyAppService;
-    }
-
     protected override async Task<IContacts> HandleCoreAsync(IRequestInput input,
         RequestGetContacts obj)
     {
-        var contactReadModels = await _queryProcessor
+        var contactReadModels = await queryProcessor
             .ProcessAsync(new GetContactsByUserIdQuery(input.UserId), CancellationToken.None);
         var userIdList = contactReadModels.Select(p => p.TargetUserId).ToList();
         userIdList.Add(input.UserId);
-        var userReadModels = await _queryProcessor
+        var userReadModels = await queryProcessor
             .ProcessAsync(new GetUsersByUidListQuery(userIdList));
-        var privacies = await _privacyAppService.GetPrivacyListAsync(userIdList);
-        var photos = await _photoAppService.GetPhotosAsync(userReadModels,contactReadModels);
-        var userList = _layeredUserService.GetConverter(input.Layer).ToUserList(input.UserId, userReadModels, photos, contactReadModels, privacies);
+        var privacies = await privacyAppService.GetPrivacyListAsync(userIdList);
+        var photos = await photoAppService.GetPhotosAsync(userReadModels, contactReadModels);
+        var userList = layeredUserService.GetConverter(input.Layer).ToUserList(input.UserId, userReadModels, photos, contactReadModels, privacies);
 
         foreach (var user in userList)
         {
             user.Contact = true;
         }
 
+        var hash = hashCalculator.GetHash(userIdList);
+
+        if (obj.Hash != 0 && obj.Hash == hash)
+        {
+            return new TContactsNotModified();
+        }
+
         var r = new TContacts
         {
             Contacts =
-                new TVector<IContact>(contactReadModels.Select(p =>
+                new TVector<IContact>(contactReadModels.Where(p => userIdList.Contains(p.TargetUserId)).Select(p =>
                     new TContact { UserId = p.TargetUserId, Mutual = false })),
             Users = new TVector<IUser>(userList),
-            SavedCount = contactReadModels.Count
+            SavedCount = contactReadModels.Count,
         };
 
         return r;
